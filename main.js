74c2bf1b06951344aefda19299084785072b0c9a
Eshankhan796 <khaneshan796@gmail.com>
2024-07-09 16:44:47 +0600

    v1.02.04

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-const topic_container = document.querySelectorAll('.a7');
-const topit_select = document.querySelectorAll('.a5 a');
-
-window.onscroll = () => {
-  topic_container.forEach(container => {
-    let top = window.scrollY;
-    let offset = container.offsetTop - 100;
-    let height = container.offsetHeight;
-    const id = container.getAttribute('id');
-    if (top >= offset && top < offset + height) {
-      topit_select.forEach(links => {
-        links.classList.remove('active');
-        document.querySelector('.a5 a[href*=' + id + ']').classList.add('active');
-      })
-    }
-  })
-};
+function add_bookmark(container_name) {
+  dialog.show();
+}
